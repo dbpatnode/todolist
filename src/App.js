fa1c99b593d0f7f9ae8403abb22edbad37578585
dbpatnodee <dbpatnode@hotmail.com>
2020-11-30 14:50:36 -0800

    addItem & updateInput functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ class App extends Component {
     newItem: "",
     list: [],
   };
+
+  updateInput = (key, value) => {
+    // update react state
+    this.setState({ [key]: value });
+  };
+
+  addItem = () => {
+    // create item w/ unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice(),
+    };
+
+    // copy of current list of items
+    const list = [...this.state.list];
+
+    // add new item to list
+    list.push(newItem);
+
+    // update state w/ new list and reset newItem
+    this.setState({
+      list,
+      newItem: "",
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,7 +39,7 @@ class App extends Component {
           <br />
           <input
             type="text"
-            placeholder="Type item..."
+            placeholder="To do..."
             value={this.state.newItem}
             onChange={(e) => this.updateInput("newItem", e.target.value)}
           />
