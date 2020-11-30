2bae689285af826fa99d2f975b6a8529b5366e62
dbpatnodee <dbpatnode@hotmail.com>
2020-11-30 14:39:07 -0800

    created form to add item to do list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    newItem: "",
+    list: [],
+  };
   render() {
-    return <div className="App">hello world!</div>;
+    return (
+      <div className="App">
+        <div>
+          Add an item:
+          <br />
+          <input
+            type="text"
+            placeholder="Type item..."
+            value={this.state.newItem}
+            onChange={(e) => this.updateInput("newItem", e.target.value)}
+          />
+          <br />
+          <button onClick={() => this.addItem()}>Add Todo</button>
+        </div>
+      </div>
+    );
   }
 }
 
